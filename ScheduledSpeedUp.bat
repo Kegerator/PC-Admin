ea85cdd7374dc74cf72229b9b60a6f8102aad83d
Kegerator <jeffscabana@gmail.com>
2023-01-30 14:23:39 -0500

    Update ScheduledSpeedUp.bat
    Updated to include clearing the print spooler

--- ScheduledSpeedUp.bat
+++ ScheduledSpeedUp.bat
@@ -33,12 +33,11 @@ Set SDay=%date:~7,2%
 Set StartTime=%Time:~0,5%
 Echo.
 Echo ********************************************************
-Echo *****  ---This will take 10 minutes to 2 hours---  *****
+Echo *****   ---This will take 5 minutes to 2 hours---  *****
 Echo *****                                              *****
 Echo ***** Deleting Temp Files                          *****
-Echo ***** Running Windows Disk Clean up Tool           *****
 Echo ***** Check Windows System Files                   *****
-Echo ***** Start System Maintenance                     *****
+Echo ***** Clear any print jobs and restart the spooler *****
 Echo ***** Flush DNS and reset IP Stack                 *****
 Echo ***** Sync time to the Internet                    *****
 Echo *****                                              *****
@@ -47,7 +46,7 @@ Echo ***** Start Time %SDay%-%SMonth%-%SYear%  %StartTime% >C:\Windows\Temp\Spee
 Echo ********************************************************
 Echo ****               Log file located at             *****
 Echo %temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt
-ping -n 10 127.0.0.1 >nul: 2>nul:
+ping -n 4 127.0.0.1 >nul: 2>nul:
 
 
 :: Deleting Temp Files
@@ -81,58 +80,35 @@ RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2 >>%temp%\SpeedUpAuto%SDay%-%SM
 ping -n 2 127.0.0.1 >nul: 2>nul:
 
 
-:: Running Windows Disk Clean up Tool
-Set TTime=%Time:~0,5%
-Echo ********************************************************
-Echo ***** Running Windows Disk Clean up Tool           *****
-Echo ***** Running Windows Disk Clean up Tool           ***** >>C:\Windows\Temp\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 
-Echo ***** 5 Minutes                                    *****
-Echo ***** Time %TTime%
-Echo ********************************************************
-cleanmgr /sagerun:1 | out-Null
-ping -n 2 127.0.0.1 >nul: 2>nul:
-
 
 :: Check Windows System Files
 Set TTime=%Time:~0,5%
-Echo ***** Check Windows System Files, 4 checks         *****
-Echo ***** Check Windows System Files, 4 checks         ***** >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 
+Echo ***** Check Windows System Files                   *****
+Echo ***** Check Windows System Files                   ***** >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 
 Echo ***** 2 Hours                                      *****
 Echo ***** Time %TTime%
 Echo *****                                              *****
-DISM /Online /Cleanup-Image /CheckHealth >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
-Set TTime=%Time:~0,5%
-Echo ***** Check #1 Done %TTime%                          *****
-ping -n 10 127.0.0.1 >nul: 2>nul:
-DISM /Online /Cleanup-Image /ScanHealth >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
-Set TTime=%Time:~0,5%
-Echo ***** Check #2 Done %TTime%                          *****
-ping -n 10 127.0.0.1 >nul: 2>nul:
-DISM /Online /Cleanup-Image /RestoreHealth >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
-Set TTime=%Time:~0,5%
-Echo ***** Check #3 Done %TTime%                          *****
-ping -n 10 127.0.0.1 >nul: 2>nul:
 sfc /scannow >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
 Set TTime=%Time:~0,5%
 Echo ***** Check #4 Done %TTime%                          *****
 Echo ********************************************************
-ping -n 10 127.0.0.1 >nul: 2>nul:
+ping -n 4 127.0.0.1 >nul: 2>nul:
 
 
-:: Start System Maintenance Silent
+:: Clear any print jobs and restart the spooler
 Set TTime=%Time:~0,5%
-Echo ***** Starting System Maintenance                  *****
-Echo ***** Starting System Maintenance                  ***** >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 
-Echo ***** 2 Minutes, then will run in the background   *****
+Echo ***** Clear any print jobs and restart the spooler *****
+Echo ***** Clear any print jobs and restart the spooler ***** >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 
+Echo ***** 1 Minute                                     *****
 Echo ***** Time %TTime%
 Echo ********************************************************
-ping -n 2 127.0.0.1 >nul: 2>nul:
-:: Enable Auto Maintenance
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance" /V "MaintenanceDisabled" /T REG_DWORD /D "0" /f >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
-ping -n 2 127.0.0.1 >nul: 2>nul:
-:: Start Maintenance
-MSchedExe.exe Start >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
-ping -n 120 127.0.0.1 >nul: 2>nul:
+:$reference https://www.dostips.com/
+:$reference https://support.microsoft.com/kb/946737
+net stop spooler /y >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
+del "%systemroot%\system32\spool\printers\*.shd" >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
+del "%systemroot%\system32\spool\printers\*.spl" >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
+net start spooler >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
+ping -n 3 127.0.0.1 >nul: 2>nul:
 
 
 ::  Flush DNS and reset IP Stack
@@ -152,7 +128,7 @@ ping -n 4 127.0.0.1 >nul: 2>nul:
 :: ipconfig /release >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
 :: ping -n 4 127.0.0.1 >nul: 2>nul:
 :: ipconfig /renew >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
-ping -n 30 127.0.0.1 >nul: 2>nul:
+ping -n 3 127.0.0.1 >nul: 2>nul:
 
 
 :: Sync time to the Internet
@@ -170,7 +146,7 @@ w32tm /config /update >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\
 w32tm /register >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
 net start w32time >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
 w32tm /resync /rediscover >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 2>>%temp%\SpeedUpAutoErrors%SDay%-%SMonth%-%SYear%.txt
-ping -n 30 127.0.0.1 >nul: 2>nul:
+ping -n 4 127.0.0.1 >nul: 2>nul:
 
 
 :: start notepad "%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt"
@@ -182,13 +158,11 @@ Set TDay=%date:~7,2%
 Set StopTime=%Time:~0,5%
 Echo.
 Echo ********************************************************
-Echo ***** Doing the last cleanup                       *****
-Echo ***** Doing the last cleanup                       ***** >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 
-Echo ***** 1 Minutes                                    *****
+Echo *****  Thank you                                   *****
+Echo *****  Thank you                                   ***** >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 
 Echo ***** Start Time %SDay%-%SMonth%-%SYear%  %StartTime% >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 
 Echo ***** Stop Time  %TDay%-%TMonth%-%TYear%  %StopTime%  >>%temp%\SpeedUpAuto%SDay%-%SMonth%-%SYear%.txt 
 Echo ***** Start Time %SDay%-%SMonth%-%SYear%  %StartTime%
 Echo ***** Stop Time  %TDay%-%TMonth%-%TYear%  %StopTime%
-Echo *****  Thank you                                   *****
 Echo ********************************************************
-ping -n 6 127.0.0.1 >nul: 2>nul
+ping -n 4 127.0.0.1 >nul: 2>nul
